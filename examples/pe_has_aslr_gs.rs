bda3358cc11406f5da6188c6cb681d453a761df8
Marco Grassi <marco.gra@gmail.com>
2022-10-09 15:24:11 +0200

    solved merge issue and rebase branch

--- examples/pe_has_aslr_gs.rs
+++ examples/pe_has_aslr_gs.rs
@@ -1,10 +1,10 @@
 extern crate checksec;
 extern crate goblin;
-extern crate memmap;
+extern crate memmap2;
 
 use checksec::pe::Properties;
 use goblin::pe::PE;
-use memmap::Mmap;
+use memmap2::Mmap;
 use std::{env, fs};
 
 fn main() {
