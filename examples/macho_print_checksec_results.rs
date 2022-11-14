f0a950de3167c6ce7083677e6017a5ec8cc106ee
Christian Göttsche <cgzones@googlemail.com>
2022-11-14 17:52:01 +0100

    Update to goblin 0.6.0
    Goblin added support for archives in Mach fat binaries, see [1].
    
    Tested against the binaries from that commit.
    
    [1]: https://github.com/m4b/goblin/commit/e2b5207a0d2dd4e8cc9111be1c1fb0a129c51632

--- examples/macho_print_checksec_results.rs
+++ examples/macho_print_checksec_results.rs
@@ -2,7 +2,10 @@ extern crate checksec;
 extern crate goblin;
 
 use checksec::macho::CheckSecResults;
-use goblin::mach::{Mach, MachO};
+use goblin::mach::cputype::get_arch_name_from_types;
+use goblin::mach::Mach;
+use goblin::mach::SingleArch::Archive;
+use goblin::mach::SingleArch::MachO;
 use goblin::Object;
 use std::{env, fs};
 
@@ -12,32 +15,60 @@ fn main() {
         2 => {
             if fs::File::open(&argv[1]).is_ok() {
                 if let Ok(buf) = fs::read(&argv[1]) {
-                    match Object::parse(&buf).unwrap() {
-                        Object::Mach(mach) => match mach {
-                            Mach::Binary(macho) => {
-                                println!(
-                                    "{:#?}",
-                                    CheckSecResults::parse(&macho)
-                                );
-                            }
-                            Mach::Fat(fatmach) => {
-                                for (idx, _) in
-                                    fatmach.iter_arches().enumerate()
-                                {
-                                    let container: MachO =
-                                        fatmach.get(idx).unwrap();
-                                    println!(
-                                        "{:#?}",
-                                        CheckSecResults::parse(&container)
-                                    );
-                                }
-                            }
-                        },
-                        _ => eprintln!("not a mach binary"),
-                    }
+                    parse(&buf);
                 }
             }
         }
         _ => eprintln!("Usage: macho_print_checksec_results <binary>"),
+    }
+}
+
+fn parse(bytes: &[u8]) {
+    match Object::parse(&bytes).unwrap() {
+        Object::Mach(mach) => match mach {
+            Mach::Binary(macho) => {
+                println!("{:#?}", CheckSecResults::parse(&macho));
+            }
+            Mach::Fat(fatmach) => {
+                for (idx, fatarch) in fatmach.iter_arches().enumerate() {
+                    match fatmach.get(idx).unwrap() {
+                        MachO(mach) => {
+                            let machine = get_arch_name_from_types(
+                                mach.header.cputype(),
+                                mach.header.cpusubtype(),
+                            )
+                            .unwrap_or("UNKNOWN");
+                            println!("# Machine type {}:", machine);
+                            println!("{:#?}", CheckSecResults::parse(&mach))
+                        }
+                        Archive(archive) => {
+                            let fatarch = fatarch.unwrap();
+
+                            let archive_bytes = &bytes[fatarch.offset as usize
+                                ..(fatarch.offset + fatarch.size) as usize];
+
+                            for member in archive.members() {
+                                match archive.extract(member, archive_bytes) {
+                                    Ok(ext_bytes) => {
+                                        println!(
+                                            "# Archive member {}:",
+                                            member
+                                        );
+                                        parse(ext_bytes);
+                                    }
+                                    Err(err) => {
+                                        eprintln!(
+                                            "Failed to extract member {}: {}",
+                                            member, err
+                                        );
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        _ => eprintln!("not a mach binary"),
     }
 }
